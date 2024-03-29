562ea8e2694a6eb350289b6443091e40065456ce
Stanislav Goroshko <stangoroshko@yandex.ru>
2024-03-29 23:40:10 +0300

    Исправления после 1 ревью.

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,15 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // DOM узлы
 const content = document.querySelector('.content');
-const addButton = content.querySelector('.profile__add-button');
+const profileAddButton = content.querySelector('.profile__add-button');
 const cardsContainer = content.querySelector('.places__list');
 
 // Функция создания карточки
 function addCard(cardData, deleteCard) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  cardElement.querySelector('.card__image').src = cardData.link;
-  cardElement.querySelector('.card__image').alt = cardData.name;
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
   cardElement.querySelector('.card__title').textContent = cardData.name;
   
   const deleteButton = cardElement.querySelector('.card__delete-button');
